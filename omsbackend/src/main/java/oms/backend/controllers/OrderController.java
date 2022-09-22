c99ac4f50bed7ef393571d7c29e045ccab15204c
Alan <lialan478@yahoo.com>
2022-09-22 13:22:56 -0700

    added comments

--- omsbackend/src/main/java/oms/backend/controllers/OrderController.java
+++ omsbackend/src/main/java/oms/backend/controllers/OrderController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.bytebuddy.asm.Advice.Return;
 import oms.backend.models.Order;
 import oms.backend.services.OrderService;
 
@@ -23,6 +24,13 @@ public class OrderController {
     @Autowired
     OrderService service;
 
+    /**This method takes in an integer id and lets us update
+     * the order to be labeled as shipped
+     * @param id an integer that reprsents the orderid of the order we want to update to shipped
+     * @return This method returns a response entity in the form a 
+     * string that specifies the order id number and if it has shipped
+     * successfully. If not, we return a bad request.
+     */
     @PutMapping("/updateOrder/shipped/{id}")
     public ResponseEntity<String> shippedOrder(@PathVariable("id") int id){
         boolean success = service.updateOrder(id, "Shipped");
@@ -33,6 +41,16 @@ public class OrderController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
+
+
+    /**
+     * This method takes in an integer id and lets us update the order to be
+     * labeled as canceled
+     * @param id an integer that represents the id of the order we want canceled
+     * @return This method returns a response entity in the form a 
+     * string that specifies the order id number and if it has been cancelled
+     * successfully. If not, we return a bad request.
+     */
     @PutMapping("/updateOrder/cancelled/{id}")
     public ResponseEntity<String> cancelledOrder(@PathVariable("id") int id){
         boolean success = service.updateOrder(id, "Canceled");
@@ -44,6 +62,14 @@ public class OrderController {
         }
     }
 
+    /**
+     * This method takes in an integer id and lets us update the order to be
+     * labeled as delivered
+     * @param id an integer that represents the id of the order we want to be laveled as delivered
+     * @return This method returns a response entity in the form a 
+     * string that specifies the order id number and if it has been delivered
+     * successfully. If not, we return a bad request.
+     */
     @PutMapping("/updateOrder/delivered/{id}")
     public ResponseEntity<String> deliveredOrder(@PathVariable("id") int id) {
         boolean success = service.updateOrder(id, "Delivered");
@@ -55,6 +81,13 @@ public class OrderController {
         }
     }
 
+    /**
+     * This method gets orders with the specified id
+     * @param id an integer that represents the id of the order we want to view
+     * @return This method returns an response entity in the form of an order
+     * that represents the order we want to view. If there are no orders, we return
+     * a bad request.
+     */
     @GetMapping("/getOrders/{id}")
     public ResponseEntity<Order> getOrderById(@PathVariable("id") int id){
         Optional <Order> order = service.getOrderById(id);
@@ -64,6 +97,11 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
 
+    /**
+     * This method gets all orders in the database and displays it
+     * @return a list with all orders in our database. If there is nothing
+     * in the database, it returns no content as the http status
+     */
     @GetMapping("/getOrders")
     public ResponseEntity<List<Order>> getOrders(){
         List<Order> orders = service.getOrders();
@@ -73,6 +111,11 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
     }
 
+    /**
+     * This method gets list of order from a particular user
+     * @param id the id of the user
+     * @return a list of orders that the user has made
+     */
     @GetMapping("/getOrders/user/{id}")
     public ResponseEntity<List<Order>> getOrderInfo(@PathVariable("id") int id) {
         List<Order> orders=service.getOrdersUserId(id);
@@ -82,6 +125,12 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(orders);
     }
     
+    /**
+     * This method allows the user to create an order and add it to the database.
+     * @param order An order item that will be added to our database
+     * @return a response entity that specifies if the order is created. If not,
+     * we return a bad request
+     */
     @PostMapping("/createOrder")
     public ResponseEntity<String> createOrder(@RequestBody Order order){
     	boolean success = service.createOrder(order);
--- omsbackend/src/main/java/oms/backend/services/OrderServiceImpl.java
+++ omsbackend/src/main/java/oms/backend/services/OrderServiceImpl.java
@@ -17,6 +17,16 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     OrderRepo repo;
 
+    /**
+     * This method allows us to update the status of an order that
+     * is already in the database. The status should be in one 
+     * of three status: "Ordered" "Shipped" "Canceled"
+     * It will also update the date status if an order has been changed.
+     * @param id the id of the order to be updated
+     * @param status the status we want to update our current order to
+     * @return true or false depending on whether or not
+     * the order was able to be updated to our new status
+     */
     @Override
     public boolean updateOrder(int id, String status) {
         Optional<Order> exists = repo.findById(id);
@@ -58,17 +68,34 @@ public class OrderServiceImpl implements OrderService {
         return false;
     }
 
+    /**
+     * This order returns an optional that will contain an order
+     * if the order of the given id exists
+     * @param id the id of the order to find
+     * @return an optional. if the order exists,
+     */
     @Override
     public Optional<Order> getOrderById(int id) {
         Optional<Order> exists = repo.findById(id);
         return exists;
     }
 
+    /**
+     * This method returns a list with all orders from the database
+     * @return a list with all orders in the order database
+     */
     @Override
     public List<Order> getOrders() {
         return repo.findAll();
     }
 
+    /**
+     * This method will insert an order into the database and
+     * set the default status of the order to pending.
+     * @param order an order item that is to be added to our database
+     * @return boolean true or false depending on whnether or not the
+     * new order was able to created
+     */
 	@Override
 	public boolean createOrder(Order order) {
 		order.setOrderStatus("PENDING");
@@ -79,6 +106,12 @@ public class OrderServiceImpl implements OrderService {
 			return false;
 		}		
 	}
+    /**
+     * This method takes in a user id and returns all orders
+     * that the user of that id has made
+     * @param id the id of the userid we want to finder orders for
+     * @return a list of orders that this user has made
+     */
     @Override
     public List<Order> getOrdersUserId(int id) {
         List<Order> orders = new ArrayList<Order>();
