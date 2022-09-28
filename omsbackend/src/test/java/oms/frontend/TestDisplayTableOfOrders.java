044790181f31b32c2070b9af3c3dc81c761be242
Quan Nguyen <71422583+AllenNguyen0417@users.noreply.github.com>
2022-09-28 15:25:01 -0700

    Create UI Testing Environment

---
+++ omsbackend/src/test/java/oms/frontend/TestDisplayTableOfOrders.java
@@ -0,0 +1,32 @@
+package oms.frontend;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestDisplayTableOfOrders {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+    }
+
+    @Test
+    void test() throws Exception{
+        driver.get("http://localhost:4200/");
+        Thread.sleep(3000);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        driver.quit();
+    }
+
+}
