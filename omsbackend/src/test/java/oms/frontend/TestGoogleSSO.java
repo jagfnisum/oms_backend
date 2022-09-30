712db9f6a5b431b73fd844861917b65d60336714
Zeeshan Mohamedy <zeeshan.mohamedy@gmail.com>
2022-09-30 14:23:18 -0700

    adding Test files

--- omsbackend/src/test/java/oms/frontend/TestGoogleSSO.java
+++ omsbackend/src/test/java/oms/frontend/TestGoogleSSO.java
@@ -1,21 +1,15 @@
 package oms.frontend;
-
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.util.Set;
-import java.util.List;
-
-
-public class TestSearchOrders {
+public class TestGoogleSSO {
     static WebDriver driver;
 
-    @BeforeAll
-    static void setUp() throws Exception {
+    @Test
+    static void Login() throws Exception {
 
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         ChromeOptions handlingSSL = new ChromeOptions();
@@ -46,49 +40,15 @@ public class TestSearchOrders {
         Thread.sleep(4000);
         driver.switchTo().window(mainwindow);
         Thread.sleep(1000);
+        String TableShown = driver.findElement(By.xpath("/html/body/app-root/order-table/div/table/thead/tr/th[1]/div/div[1]")).getText();
+        Assertions.assertEquals("Orderid", TableShown);
 //        driver.findElement(By.xpath("//*[@id=\"mat-select-value-1\"]")).click();
 //        Thread.sleep(1000);
 //        driver.findElement(By.xpath("//*[@id=\"mat-option-4\"]/span")).click();
 
     }
-
-    @Test
-    void searchOrder() throws Exception{
-        String testInput = "Del";
-        int count =0;
-        driver.findElement(By.tagName("input")).sendKeys(testInput);
-        Thread.sleep(1000);
-            List<WebElement> rows_table = driver.findElements(By.tagName("tr"));
-            //To calculate no of rows In table.
-            rows_table.remove(0);
-            int rows_count = rows_table.size();
-
-            //Loop will execute till the last row of table.
-            for (int row=0; row<rows_count; row++){
-                //To locate columns(cells) of that specific row.
-                List<WebElement> Columns_row = rows_table.get(row).findElements(By.tagName("td"));
-                //To calculate no of columns(cells) In that specific row.
-                int columns_count = Columns_row.size();
-
-                //Loop will execute till the last cell of that specific row.
-                for (int column=0; column<columns_count; column++) {
-                    //To retrieve text from that specific cell.
-                    String celtext = Columns_row.get(column).getText();
-                    System.out.print(celtext + " ");
-                    if (celtext.contains(testInput)) {
-                        count++;
-                        System.out.println("found");
-                    }
-//
-                }
-            }
-        System.out.println(count);
-        Assertions.assertEquals(rows_count,count);
-    }
-
     @AfterEach
     void tearDown() throws Exception {
         driver.quit();
     }
-
 }
