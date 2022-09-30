7b66b022fdefd9c601b3ac376b08ff0e3909a322
Quan Nguyen <71422583+AllenNguyen0417@users.noreply.github.com>
2022-09-30 10:07:21 -0700

    Adding Test Search Order

--- omsbackend/src/test/java/oms/frontend/TestSearchOrders.java
+++ omsbackend/src/test/java/oms/frontend/TestSearchOrders.java
@@ -1,32 +1,66 @@
 package oms.frontend;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class TestSearchOrders {
 
-    WebDriver driver;
+    static WebDriver driver;
 
-    @BeforeEach
-    void setUp() throws Exception {
+    @BeforeAll
+    static void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        driver.get("http://localhost:4200/");
+        Thread.sleep(1000);
+        driver.findElement(By.className("google-login-btn")).click();
+        Thread.sleep(1000);
+
+        String mainwindow = driver.getWindowHandle();
+        Set<String> windows = driver.getWindowHandles();
+
+        for(String window: windows) {
+            if(window.equals(mainwindow) == false) {
+                driver.switchTo().window(window);
+            }
+        }
+        driver.findElement(By.id("identifierId")).sendKeys("nisumuitesting");
+        Thread.sleep(1000);
+        driver.findElement(By.id("identifierNext")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.name("password")).sendKeys("passWord!");
+        Thread.sleep(1000);
+        driver.findElement(By.id("passwordNext")).click();
+        Thread.sleep(4000);
+        driver.switchTo().window(mainwindow);
     }
 
     @Test
     void test() throws Exception{
-        driver.get("http://localhost:4200/");
-        Thread.sleep(3000);
+        driver.findElement(By.name("Search")).sendKeys("Shipped");
+        String shippedString = driver.findElement(By.xpath("//order-table/div/table/tbody/tr[1]/td[6]")).getText();
+        Assertions.assertEquals("Shipped", shippedString);
+        Thread.sleep(1000);
+
+        driver.findElement(By.name("Search")).clear();
+        Thread.sleep(1000);
+
+        driver.findElement(By.name("Search")).sendKeys("Delivered");
+        Thread.sleep(1000);
+        String deliveredString = driver.findElement(By.xpath("//order-table/div/table/tbody/tr[1]/td[6]")).getText();
+        Assertions.assertEquals("Delivered", deliveredString);
+
     }
 
     @AfterEach
     void tearDown() throws Exception {
-        driver.quit();
+//        driver.quit();
     }
 
 }
